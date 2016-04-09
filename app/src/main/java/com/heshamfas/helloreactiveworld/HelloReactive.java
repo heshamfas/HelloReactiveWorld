bc48367b94a3f260b735b4f11e5a044a7be20354
Hesham Fas <hfasdev@gmail.com>
2016-04-08 18:12:54 -0700

    Initial commit

---
+++ app/src/main/java/com/heshamfas/helloreactiveworld/HelloReactive.java
@@ -0,0 +1,13 @@
+package com.heshamfas.helloreactiveworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HelloReactive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello_reactive);
+    }
+}
